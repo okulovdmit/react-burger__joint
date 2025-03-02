9a0571342f622aef3bc59344fa676f66eb4eb057
Dmitrii Okulov <okulovdmit@yandex.ru>
2025-03-02 18:59:58 +0500

    feat(component/burger-ingredients): add highlight nav on Scroll change

--- src/component/burger-ingredients/burger-ingredients.jsx
+++ src/component/burger-ingredients/burger-ingredients.jsx
@@ -1,42 +1,59 @@
+import { useState } from 'react';
 import * as PropTypes from 'prop-types';
 import sIngredients from './burger-ingredients.module.scss';
 import { IngredientCard } from '../ingredient-card/ingredient-card';
 import { ingredientPropType } from '../../utils/prop-type';
 import { Tabs } from '../tabs/tabs';
+import { Section } from '../tabs/section';
 
 const BurgerIngredients = ({ ingredients, toggle, getProduct }) => {
 	const bunFilter = ingredients.filter((item) => item.type === 'bun');
 	const sauceFilter = ingredients.filter((item) => item.type === 'sauce');
 	const mainFilter = ingredients.filter((item) => item.type === 'main');
+
+	const [activeTab, setActiveTab] = useState('булки');
+
 	return (
 		<section className={sIngredients.main}>
 			<h1 className={'text text_type_main-large mt-10'}>Соберите бургер</h1>
-			<Tabs />
+			<Tabs activeTab={activeTab} setActiveTab={setActiveTab} />
 			<div className={`${sIngredients.ingredients} custom-scroll`}>
-				<h2 className={'text text_type_main-medium mt-10'}>Булки</h2>
-				<div className={sIngredients.cards}>
+				<Section
+					id='булки'
+					classname={sIngredients.cards}
+					onVisibilityChange={setActiveTab}
+					label='Булки'>
 					<IngredientCard
 						data={bunFilter}
 						toggle={toggle}
 						getProduct={getProduct}
 					/>
-				</div>
-				<h2 className={'text text_type_main-medium mt-10'}>Соусы</h2>
-				<div className={sIngredients.cards}>
+				</Section>
+
+				<Section
+					id='соусы'
+					classname={sIngredients.cards}
+					onVisibilityChange={setActiveTab}
+					label='Соусы'>
 					<IngredientCard
 						data={sauceFilter}
+						classname={sIngredients.cards}
 						toggle={toggle}
 						getProduct={getProduct}
 					/>
-				</div>
-				<h2 className={'text text_type_main-medium mt-10'}>Начинки</h2>
-				<div className={sIngredients.cards}>
+				</Section>
+
+				<Section
+					id='начинки'
+					classname={sIngredients.cards}
+					label='Начинки'
+					onVisibilityChange={setActiveTab}>
 					<IngredientCard
 						data={mainFilter}
 						toggle={toggle}
 						getProduct={getProduct}
 					/>
-				</div>
+				</Section>
 			</div>
 		</section>
 	);
--- src/component/tabs/tabs.jsx
+++ src/component/tabs/tabs.jsx
@@ -1,18 +1,40 @@
 import sTabs from './tabs.module.scss';
 import { Tab } from '@ya.praktikum/react-developer-burger-ui-components';
-import { useState } from 'react';
 
-export const Tabs = () => {
-	const [current, setCurrent] = useState('one');
+export const Tabs = ({ activeTab, setActiveTab }) => {
 	return (
-		<div className={`${sTabs.tabs} mt-5 `}>
-			<Tab value='one' active={current === 'one'} onClick={setCurrent}>
+		<div className={`${sTabs.tabs} mt-5`}>
+			<Tab
+				value='булки'
+				active={activeTab === 'булки'}
+				onClick={() => {
+					setActiveTab('булки');
+					document
+						.getElementById('булки')
+						.scrollIntoView({ behavior: 'smooth' });
+				}}>
 				Булки
 			</Tab>
-			<Tab value='two' active={current === 'two'} onClick={setCurrent}>
+			<Tab
+				value='соусы'
+				active={activeTab === 'соусы'}
+				onClick={() => {
+					setActiveTab('соусы');
+					document
+						.getElementById('соусы')
+						.scrollIntoView({ behavior: 'smooth' });
+				}}>
 				Соусы
 			</Tab>
-			<Tab value='three' active={current === 'three'} onClick={setCurrent}>
+			<Tab
+				value='начинки'
+				active={activeTab === 'начинки'}
+				onClick={() => {
+					setActiveTab('начинки');
+					document
+						.getElementById('начинки')
+						.scrollIntoView({ behavior: 'smooth' });
+				}}>
 				Начинки
 			</Tab>
 		</div>
