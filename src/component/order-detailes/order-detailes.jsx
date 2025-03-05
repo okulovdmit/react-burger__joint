843be41fc9b9bae7d8459d85e357c03c784b6096
Dmitrii Okulov <okulovdmit@yandex.ru>
2025-03-05 23:37:09 +0500

    style(component/modal and order-detailes): fix some styles, add margin botom in order-detailes

--- src/component/order-detailes/order-detailes.jsx
+++ src/component/order-detailes/order-detailes.jsx
@@ -20,7 +20,10 @@ export default function OrderDetailes({ toggle, number }) {
 			<p className={'text text_type_main-default mt-15'}>
 				Ваш заказ начали готовить
 			</p>
-			<p className={'text text_type_main-default text_color_inactive mt-2'}>
+			<p
+				className={
+					'text text_type_main-default text_color_inactive mt-2 mb-15'
+				}>
 				Дождитесь готовности на орбитальной станции
 			</p>
 		</div>
