5d3b15d30b9a7af398a43941ba0e0d944d803945
Dmitrii Okulov <okulovdmit@yandex.ru>
2025-04-22 13:10:10 +0500

    feat(services/feedprofile): created actions for feedProfile

---
+++ src/services/feedProfile/actions.ts
@@ -0,0 +1,31 @@
+import { createAction } from '@reduxjs/toolkit';
+import { TFeedData } from '@utils/types';
+
+export const connectProfile = createAction<
+	string,
+	'feedProfile/connectProfile'
+>('feedProfile/connectProfile');
+export const disconnectProfile = createAction('feedProfile/disconnectProfile');
+
+export const onConnectingProfile = createAction(
+	'feedProfile/onConnectingProfile'
+);
+export const onOpenProfile = createAction('feedProfile/onOpen');
+export const onCloseProfile = createAction('feedProfile/onCloseProfile');
+export const onErrorProfile = createAction<
+	string,
+	'feedProfile/onErrorProfile'
+>('feedProfile/onErrorProfile');
+export const onMessageProfile = createAction<
+	TFeedData,
+	'feedProfile/onMessage'
+>('feedProfile/onMessage');
+
+export type FeedProfileActions =
+	| ReturnType<typeof connectProfile>
+	| ReturnType<typeof disconnectProfile>
+	| ReturnType<typeof onConnectingProfile>
+	| ReturnType<typeof onOpenProfile>
+	| ReturnType<typeof onCloseProfile>
+	| ReturnType<typeof onErrorProfile>
+	| ReturnType<typeof onMessageProfile>;
